0325c701961d77c671312c9c41fa2fa80d5f224c
Pavel Kulagov <kulagov@yandex-team.ru>
2022-09-15 20:53:01 +0300

    Add de08030501

--- settings.py
+++ settings.py
@@ -1,2 +1,4 @@
-USER_HOST = 'xx.xx.xx.xx' # укажите свой хост
-TOPIC_NAME = 'student.topic.cohort<номер когорты>.<username>' # укажите название топика student.topic.cohort<номер когорты>.<username>
+# USER_HOST = 'xx.xx.xx.xx' # укажите свой хост
+# TOPIC_NAME = 'student.topic.cohort<номер когорты>.<username>' # укажите название топика student.topic.cohort<номер когорты>.<username>
+USER_HOST = '51.250.88.217' # укажите свой хост
+TOPIC_NAME = 'student.topic.cohort00.Test1' # укажите название топика student.topic.cohort<номер когорты>.<username>
