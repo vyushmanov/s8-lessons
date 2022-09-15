ac4056ebaedd23a0f802da5d6776b43fba4a47e8
Pavel Kulagov <kulagov@yandex-team.ru>
2022-09-16 00:47:07 +0300

    Upd de08030501

---
+++ Тема 3. Настройка потока данных/5. Дедупликация/Задание 1/realization.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import functions as f
+from pyspark.sql.types import StructType, StructField, DoubleType, StringType
+
+# необходимая библиотека с идентификатором в maven
+# вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
+kafka_lib_id =
+        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+
+# настройки security для кафки
+# вы можете использовать из с помощью метода .options(**kafka_security_options)
+kafka_security_options = {
+    'kafka.security.protocol': 'SASL_SSL',
+    'kafka.sasl.mechanism': 'SCRAM-SHA-512',
+    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";',
+}
+
+def spark_init() -> SparkSession:
+    pass
+
+
+def load_df(spark: SparkSession) -> DataFrame:
+    pass
+
+
+def transform(df: DataFrame) -> DataFrame:
+    pass
+
+
+spark = spark_init()
+
+source_df = load_df(spark)
+output_df = transform(source_df)
+output_df.printSchema()
+
+query = (output_df
+         .writeStream
+         .outputMode("append")
+         .format("console")
+         .option("truncate", False)
+         .trigger(once=True)
+         .start())
+try:
+    query.awaitTermination()
+finally:
+    query.stop()
