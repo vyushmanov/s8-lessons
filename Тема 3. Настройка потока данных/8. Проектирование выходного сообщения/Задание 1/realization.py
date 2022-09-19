904a455c8c0dd7b170b23b54c4c0be7b4374e4d2
Pavel Kulagov <kulagov@yandex-team.ru>
2022-09-20 00:56:57 +0300

    add de08030801

---
+++ Тема 3. Настройка потока данных/8. Проектирование выходного сообщения/Задание 1/realization.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import functions as f
+from pyspark.sql.types import StructType, StructField, DoubleType, StringType, TimestampType, IntegerType
+
+
+def spark_init(test_name) -> SparkSession:
+	  pass
+
+postgresql_settings = {
+    'user': 'master',
+    'password': 'de-master-password'
+}
+
+
+def read_marketing(spark: SparkSession) -> DataFrame:
+    pass
+
+
+
+kafka_security_options = {
+    'kafka.security.protocol': 'SASL_SSL',
+    'kafka.sasl.mechanism': 'SCRAM-SHA-512',
+    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"kafka-admin\" password=\"de-kafka-admin-2022\";',
+}
+
+
+def read_client_stream(spark: SparkSession) -> DataFrame:
+    pass
+
+
+def join(user_df, marketing_df) -> DataFrame:
+    pass
+
+
+if __name__ == "__main__":
+    spark = spark_init('join stream')
+    client_stream = read_client_stream(spark)
+    marketing_df = read_marketing(spark)
+    result = join(client_stream, marketing_df)
+
+    query = (result
+             .writeStream
+             .outputMode("append")
+             .format("console")
+             .option("truncate", False)
+             .start())
+    query.awaitTermination()
